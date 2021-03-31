46f01a0882430cc9496aff5b01a9566b0167ce92
leomatt547 <leo.matt.547@gmail.com>
2021-03-31 21:59:27 +0700

    push readfile go

--- src/main.go
+++ src/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
 	"log"
+	"os"
+	//"strings"
 )
 
-func main() {
-	content, err := ioutil.ReadFile("../test/graph.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+type Simpul struct {
+	Lat, Long float64
+}
 
-	fmt.Printf("File contents: %s", content)
+var Kamus map[Simpul]int
+var Node int
+var DataNode [][]string
 
+
+/*func input(filename string) [][]string {
+	var hasil [...][...]string
+	
+}*/
+
+func main(){
+	file, err := os.Open("../test/graph.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+		//s := strings.Split(scanner.Text(), ",") clean komma
+		//fmt.Println(s)
+        fmt.Println(scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
 }
